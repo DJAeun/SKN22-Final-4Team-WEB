dc8725be5185ce7198a9a6aa45cb084a3226fdae
gksshing <gksshing@gmail.com>
2026-03-17 21:17:40 +0900

    fix: change tp sslmode=require

--- setup_db_schema.py
+++ setup_db_schema.py
@@ -18,7 +18,8 @@ def get_connection():
         port=DB_PORT,
         database=DB_NAME,
         user=DB_USER,
-        password=DB_PASSWORD
+        password=DB_PASSWORD,
+        sslmode='require'
     )
 
 @traceable(name="create_pgvector_extension")
